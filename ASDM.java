21f14e012d4b3bcc0c5d277e3014c030ae8a050b
Shivani8-byte <67108056+Shivani8-byte@users.noreply.github.com>
2020-07-04 20:42:05 +0530

    Write a simple  java program
    Write a simple  java program for Addition, Subtraction, Division, Multiplication

---
+++ ASDM.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+		 
+public class ASDM
+{
+    public static void main(String args[])
+    {
+       int a, b, c;
+       Scanner scan = new Scanner(System.in);
+	   
+       System.out.print("Enter Two Numbers : ");
+       a = scan.nextInt();
+       b = scan.nextInt();
+	   
+       c = a + b;
+       System.out.println("Addition = " +c);
+	   
+       c = a - b;
+       System.out.println("Subtraction = " +c);
+	   
+       c= a * b;
+       System.out.println("Multiplication = " +c);
+	   
+       c = a / b;
+       System.out.println("Division = " +c);
+    }
+}
